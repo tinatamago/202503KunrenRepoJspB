8b366c1520b086f5f12998098f57ac945fbf1db9
chinami <chinami@192.168.1.2>
2025-04-23 22:08:39 +0900

    change:DBサンプルデータ修正

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -48,11 +48,10 @@ CREATE TABLE public.ptcg_expansions (
 
 -- m_user
 INSERT INTO public.m_user (user_id, user_pass, user_name) VALUES
-(1, 1, 'たろう'),
-(2, 22, 'じろう'),
-(3, 333, 'さぶろう'),
-(4, 4444, 'しろう'),
-(5, 55555, 'ごろう');
+    (1,1,'ゲスト1')
+  , (2,22,'ゲスト2')
+  , (3,333,'ゲスト3');
+
 
 -- ptcg_categories
 INSERT INTO public.ptcg_categories (category_id, category_name) VALUES
@@ -65,19 +64,37 @@ INSERT INTO public.ptcg_categories (category_id, category_name) VALUES
 
 -- ptcg_expansions
 INSERT INTO public.ptcg_expansions (expansion_code, expansion_name, release_date) VALUES
-('sv1a','トリプレットビート',DATE '2024-03-10'),
-('sv1S','スカーレットex',DATE '2024-01-20'),
-('sv1V','バイオレットex',DATE '2024-01-20'),
-('sv2a','イチゴーイチ',DATE '2024-06-16'),
-('sv2D','クレイバースト',DATE '2024-04-14'),
-('sv2P','スノーハザード',DATE '2024-04-14');
+    ('sv1S','スカーレットex',DATE '2023-01-20')
+  , ('sv1V','バイオレットex',DATE '2023-01-20')
+  , ('sv1a','トリプレットビート',DATE '2023-03-10')
+  , ('sv2D','クレイバースト',DATE '2023-04-14')
+  , ('sv2P','スノーハザード',DATE '2023-04-14')
+  , ('sv2a','イチゴーイチ',DATE '2023-06-16')
+  , ('sv3','黒炎の支配者',DATE '2023-07-28')
+  , ('sv3a','レイジングサーフ',DATE '2023-09-22')
+  , ('sv4K','古代の咆哮',DATE '2023-10-27')
+  , ('sv4M','未来の一閃',DATE '2023-10-27')
+  , ('sv4a','シャイニートレジャーex',DATE '2023-12-01')
+  , ('sv5K','サイバージャッジ',DATE '2024-01-26')
+  , ('sv5M','ワイルドフォース',DATE '2024-01-26')
+  , ('sv5a','クリムゾンヘイズ',DATE '2024-03-22')
+  , ('sv6','変幻の仮面',DATE '2024-04-26')
+  , ('sv6a','ナイトワンダラー',DATE '2024-06-07')
+  , ('sv7','ステラミラクル',DATE '2024-07-19')
+  , ('sv7a','楽園ドラゴーナ',DATE '2024-09-17')
+  , ('sv8','超電ブレイカー',DATE '2024-10-18')
+  , ('sv9','バトルパートナーズ',DATE '2025-01-24')
+  , ('svABC','テストABC',DATE '2025-02-20')
+  , ('sv9a','熱風のアリーナ',DATE '2025-03-14')
+  , ('sv00','架空のパック',DATE '2025-04-01')
+  , ('sv10','ロケット団の栄光',DATE '2025-04-18');
 
 -- ptcg_cards
 INSERT INTO public.ptcg_cards (card_id, card_name, category_id, expansion_code, image_file_name, registration_time, update_time, price) VALUES
-    (0,'ピカチュウ',0,'sv2a','pikachu.jpg', now(), now(),200)
-  , (1,'モンスターボール',1,'sv1S','pokeball.jpg', now(), now(),100)
-  , (2,'げんきのハチマキ',2,'sv1V','card_image_00002.png', now(), now(),480)
-  , (3,'博士の研究',3,'sv1a','professor.jpg', now(), now(),20)
-  , (4,'エキサイトスタジアム',4,'sv2P','stadium.png', now(), now(),80)
-  , (5,'ジェットエネルギー',5,'sv2P','energy.png', now(), now(),null)
-  , (6,'リザードン',0,'sv2a','charizard.png', now(), now(),550);
+    (0,'ピカチュウ',0,'sv2a','pikachu.png', now(), now(),80)
+  , (1,'モンスターボール',1,'svM','pokeball.png', now(), now(),30)
+  , (2,'まけんきハチマキ',2,'sv1S','defiance_band.png', now(), now(),280)
+  , (3,'博士の研究',3,'sv4a','professor.png', now(), now(),20)
+  , (4,'エキサイトスタジアム',4,'sv8','stadium.png', now(), now(),80)
+  , (5,'ジェットエネルギー(UR)',5,'sv7a','energy.png', now(), now(),1600)
+  , (6,'リザードンex',0,'sv3','charizard_ex.png', now(), now(),330);
